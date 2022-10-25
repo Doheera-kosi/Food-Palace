313f2a11da9761cd1b9b2eab0491bfe19fdce136
Doheera-kosi <kupourevans99@gmail.com>
2022-10-25 13:04:56 +0000

    Update index.html file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 // import _ from 'lodash';
 import './style.css';
+
+const fetchData = async () => {
+  const res = await fetch(`https://www.themealdb.com/api/json/v1/1/categories.php`);
+  const data = await res.json();
+};
+fetchData();
