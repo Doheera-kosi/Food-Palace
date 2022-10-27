b77120deccebb03dfadeb3c0cafe1dfbb93b4d48
Doheera-kosi <kupourevans99@gmail.com>
2022-10-27 13:28:15 +0000

    Added Likes feature

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// import _ from 'lodash';
+import getLikes, { addLikes } from './modules/likes.js';
+import render from './modules/render.js';
 import './style.css';
 
 const element = document.querySelector('#container');
@@ -7,30 +8,28 @@ const mealUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php';
 const fetchData = async (url) => {
   const res = await fetch(url);
   const data = await res.json();
-  render(data);
-  return data;
+  const likes = await getLikes();
+  const storage = [];
+  data.categories.forEach((item) => {
+    storage.push({
+      ...(likes.find((innerItem) => innerItem.item_id.toString() === item.idCategory.toString())),
+      ...item,
+    });
+  });
+  return storage;
 };
-fetchData(mealUrl);
 
-const render = (data) => {
-  const { categories } = data;
-  categories.forEach((cat) => {
-    element.innerHTML += `
-    <div id="card-a" class="card-a">
-      <img src="${cat.strCategoryThumb}" alt="Picture">
-      <div class="space-like">
-        <p>Space</p>
-        <i class="fa fa-heart" aria-hidden="true"></i>
-      </div>
-      <div class="like">
-        <label for="text">0</label>
-        <label for="text">Likes</label>
-      </div>
-      <div class="btns">
-        <button type="button" id="comment" class="comment">Comments</button>
-        <button type="button" id="reserve" class="reserve">Reserve</button>
-      </div>
-    </div>
-  `;
+window.onload = async () => {
+  const storage = await fetchData(mealUrl);
+  render(storage, element);
+  const likeButtons = document.querySelectorAll('.fa-heart');
+  const likeBtns = Array.from(likeButtons);
+  likeBtns.forEach((btn) => {
+    btn.addEventListener('click', async (e) => {
+      const likeContainer = e.target.parentNode.parentNode.querySelector('.show');
+      await addLikes(e.target.id);
+      const newVal = +likeContainer.innerHTML + 1;
+      likeContainer.innerHTML = newVal;
+    });
   });
 };
--- src/modules/configuration.js
+++
@@ -1,3 +0,0 @@
-import foodItems from './foodItems.js';
-
-const mealsUrl = 'https://www.themealdb.com/api/json/v1/1/categories.php';
